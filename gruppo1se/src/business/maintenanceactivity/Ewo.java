3e8f8092d88e7fe97b356f6d4d1feba8b7cdbcea
VincenzaCoppola <v.coppola38@studenti.unisa.it>
2020-11-23 18:02:19 +0100

    Commit Ewo and ExtraActivity

---
+++ gruppo1se/src/business/maintenanceactivity/Ewo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.maintenanceactivity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author VincenzaCoppola <v.coppola38@studenti.unisa.it>
+ */
+public class Ewo extends UnplannedMaintenanceActivity {
+
+    public Ewo(int activityId, Site site, String typology, String activityDescription, int estimatedInterventionTime, LocalDate date, MaintenanceProcedure maintenanceProcedure, List<String> materials, boolean interruptibleActivity) {
+        super(activityId, site, typology, activityDescription, estimatedInterventionTime, date, maintenanceProcedure, materials, interruptibleActivity);
+    }
+    
+}
