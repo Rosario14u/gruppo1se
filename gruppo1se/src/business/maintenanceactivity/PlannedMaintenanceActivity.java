b25c5674b54a3a4f51348c667e83e92335ad9acc
Rosario14u <r.gaeta14@studenti.unisa.it>
2020-12-01 19:05:39 +0100

    Commit next merge

--- gruppo1se/src/business/maintenanceactivity/PlannedMaintenanceActivity.java
+++ gruppo1se/src/business/maintenanceactivity/PlannedMaintenanceActivity.java
@@ -23,4 +23,8 @@ public class PlannedMaintenanceActivity extends MaintenanceActivity{
         return "PlannedMaintenanceActivity" + super.toString();
     }
     
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj) && PlannedMaintenanceActivity.class.isInstance(obj);
+}
 }
