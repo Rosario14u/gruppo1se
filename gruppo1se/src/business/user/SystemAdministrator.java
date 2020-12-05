845c66924cef81982b9247bf3eb96f4233aa0721
Rosario14u <r.gaeta14@studenti.unisa.it>
2020-12-05 19:16:04 +0100

    Commit merge SystemAdministrator

---
+++ gruppo1se/src/business/user/SystemAdministrator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.user;
+
+import business.maintenanceactivity.MaintenanceProcedure;
+import exception.ProcedureException;
+import persistence.maintenanceactivity.MaintenanceProcedureDAO;
+
+/**
+ *
+ * @author rosar
+ */
+public class SystemAdministrator extends User {
+    private MaintenanceProcedureDAO procedureDao;
+    public SystemAdministrator(String username, String password,MaintenanceProcedureDAO procedureDao) {
+        super(username, password);
+        this.procedureDao = procedureDao; 
+    }
+    
+    public boolean saveSmpProcedure(String smp) throws ProcedureException{
+        if (smp != null){
+           return procedureDao.addSmp(new MaintenanceProcedure(smp)); 
+        }else{
+            throw new ProcedureException("Error in procedure creation");
+        }
+        
+    }
+}
