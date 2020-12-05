3d88f33ebe318e141ecc0eac3dc4dcdade014bcf
vcoppola38 <v.coppola38@studenti.unisa.it>
2020-12-05 14:55:00 +0100

    Commit methods in classes UsersDAO and UsersDAOTest

--- gruppo1se/src/persistence/user/UsersDAO.java
+++ gruppo1se/src/persistence/user/UsersDAO.java
@@ -9,6 +9,8 @@ import persistence.database.ConnectionDB;
 import business.user.*;
 import java.sql.*;
 import exception.UsersException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -16,6 +18,9 @@ import exception.UsersException;
  */
 public class UsersDAO {
     private final String SQL_INSERT = "INSERT INTO USERS (username, password, role) VALUES (?,?,?)";
+    private final String SQL_SELECT_USERNAME = "SELECT * FROM USERS WHERE USERNAME = ?";
+    private final String SQL_SELECT_ROLE = "SELECT * FROM USERS WHERE ROLE = ?";
+    
     
     public boolean addUser(User user) throws UsersException{
         try {
@@ -35,5 +40,46 @@ public class UsersDAO {
             throw new UsersException();
         }
     }
+ //===============================================================================================================================================
     
+    
+        public List<User> readUser(String username, String role) throws UsersException{
+        try {
+            Connection conn = ConnectionDB.getInstanceConnection().getConnection();
+            PreparedStatement stmt = null;
+            if (username == null && role == null)
+                return null;
+            if (username == null){
+                stmt = conn.prepareStatement(SQL_SELECT_ROLE);
+                stmt.setString(1, role);
+                ResultSet set = stmt.executeQuery();
+                return this.makeUsers(set);
+            }
+            else{ 
+                stmt = conn.prepareStatement(SQL_SELECT_USERNAME);
+                stmt.setString(1, username);
+                ResultSet set = stmt.executeQuery();
+                return this.makeUsers(set);
+            }
+        } catch (SQLException ex) {
+            throw new UsersException();
+        }
+    }
+    
+    private List<User> makeUsers(ResultSet set) throws UsersException{
+        List<User> users = new ArrayList<>();
+        try {
+            while (set.next()){
+                if (set.getString("role").equals("System Administrator"))
+                    users.add(new SystemAdministrator(set.getString("username"), set.getString("password")));
+                else if (set.getString("role").equals("Maintainer"))
+                    users.add(new Maintainer(set.getString("username"), set.getString("password")));
+                else
+                    users.add(new Planner(set.getString("username"), set.getString("password"), null, null));
+            }
+            return users;
+        } catch (SQLException ex) {
+            throw new UsersException("Error on select query");
+        }
+    }
 }
--- gruppo1se/test/persistence/user/UsersDAOTest.java
+++ gruppo1se/test/persistence/user/UsersDAOTest.java
@@ -8,6 +8,8 @@ package persistence.user;
 import business.user.*;
 import exception.UsersException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.AfterClass;
@@ -118,6 +120,122 @@ public class UsersDAOTest{
         verify(selectUserDefault(stmt, "ProvaUsername"), user);
         conn.rollback();
     }
+//===============================================================================================================================================
     
+    private void insertUserDefault(Statement stm, User user) throws SQLException{
+        String insert = "INSERT INTO Users VALUES ('"+user.getUsername()+"','"+user.getPassword()+"'";
+        if (user.getClass().getSimpleName().equals("Planner"))
+            insert+=",'Planner');";
+        else if (user.getClass().getSimpleName().equals("SystemAdministrator"))
+            insert+=",'System Administrator');";
+        else 
+            insert+=",'Maintainer');";
+        stm.executeUpdate(insert);
+    }    
+    /**
+     * Test of readUser method, of class UsersDAO.
+     */
+    @Test
+    public void testReadUserRolePlanner() throws UsersException, SQLException{
+        System.out.println("readUserPlanner");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new Planner("Planner1","PwdPlanner1",null,null));
+        expectedList.add(new Planner("Planner2","PwdPlanner2",null,null));
+        expectedList.add(new Planner("Planner3","PwdPlanner3",null,null));
+        List<User> plannerList = new ArrayList<>();
+        insertUserDefault(stm,new Planner("Planner1","PwdPlanner1",null,null));
+        insertUserDefault(stm,new Planner("Planner2","PwdPlanner2",null,null));
+        insertUserDefault(stm,new Planner("Planner3","PwdPlanner3",null,null));
+        plannerList = instance.readUser(null,"Planner");
+        assertEquals(true,plannerList.equals(expectedList));
+        conn.rollback();
+    }
+    
+        @Test
+    public void testReadUserRoleSystemAdministrator() throws UsersException, SQLException{
+        System.out.println("readUserSystemAdministrator");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new SystemAdministrator("SystemAdministrator1","PwdSystemAdministrator1"));
+        expectedList.add(new SystemAdministrator("SystemAdministrator2","PwdSystemAdministrator2"));
+        expectedList.add(new SystemAdministrator("SystemAdministrator3","PwdSystemAdministrator3"));
+        List<User> systemAdministratorList = new ArrayList<>();
+        insertUserDefault(stm,new SystemAdministrator("SystemAdministrator1","PwdSystemAdministrator1"));
+        insertUserDefault(stm,new SystemAdministrator("SystemAdministrator2","PwdSystemAdministrator2"));
+        insertUserDefault(stm,new SystemAdministrator("SystemAdministrator3","PwdSystemAdministrator3"));
+        systemAdministratorList = instance.readUser(null,"System Administrator");
+        assertEquals(true,systemAdministratorList.equals(expectedList));
+        conn.rollback();
+    }
+        @Test
+    public void testReadUserRoleMaintainer() throws UsersException, SQLException{
+        System.out.println("readUserMaintainer");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new Maintainer("Maintainer1","PwdMaintainer1"));
+        expectedList.add(new Maintainer("Maintainer2","PwdMaintainer2"));
+        expectedList.add(new Maintainer ("Maintainer3","PwdMaintainer3"));
+        List<User> maintainerList = new ArrayList<>();
+        insertUserDefault(stm,new Maintainer("Maintainer1","PwdMaintainer1"));
+        insertUserDefault(stm,new Maintainer("Maintainer2","PwdMaintainer2"));
+        insertUserDefault(stm,new Maintainer("Maintainer3","PwdMaintainer3"));
+        maintainerList = instance.readUser(null,"Maintainer");
+        assertEquals(true,maintainerList.equals(expectedList));
+        conn.rollback();
+    }
+    
+    @Test
+    public void testReadUserUsernamePlanner() throws UsersException, SQLException{
+        System.out.println("readUsernamePlanner");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new Planner("Planner1","PwdPlanner1",null,null));
+        List<User> plannerList = new ArrayList<>();
+        insertUserDefault(stm,new Planner("Planner1","PwdPlanner1",null,null));
+        plannerList = instance.readUser("Planner1",null);
+        assertEquals(true,plannerList.equals(expectedList));
+        conn.rollback();
+    }
+    
+    @Test
+    public void testReadUserUsernameSystemAdministrator() throws UsersException, SQLException{
+        System.out.println("readUsernameSystemAdministrator");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new SystemAdministrator("SystemAdministrator1","PwdSystemAdministrator1"));
+        List<User> systemAdministratorList = new ArrayList<>();
+        insertUserDefault(stm,new SystemAdministrator("SystemAdministrator1","PwdSystemAdministrator1"));
+        systemAdministratorList = instance.readUser("SystemAdministrator1",null);
+        assertEquals(true,systemAdministratorList.equals(expectedList));
+        conn.rollback();
+    }
+    
+    @Test
+    public void testReadUserUsernameMaintainer() throws UsersException, SQLException{
+        System.out.println("readUsernameMaintainer");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new Maintainer("Maintainer1","PwdMaintainer1"));
+        List<User> maintainerList = new ArrayList<>();
+        insertUserDefault(stm,new Maintainer("Maintainer1","PwdMaintainer1"));
+        maintainerList = instance.readUser("Maintainer1",null);
+        assertEquals(true,maintainerList.equals(expectedList));
+        conn.rollback();
+    }
+    
+        
+    @Test
+    public void testReadUserWrongUsernameMaintainer() throws UsersException, SQLException{
+        System.out.println("readUsernameMaintainer");
+        Statement stm = conn.createStatement();
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new Maintainer("Maintainer1","PwdMaintainer1"));
+        List<User> maintainerList = new ArrayList<>();
+        insertUserDefault(stm,new Maintainer("Maintainer1","PwdMaintainer1"));
+        maintainerList = instance.readUser("Maintainer2",null);
+        assertEquals(false,maintainerList.equals(expectedList));
+        conn.rollback();
+    }
     
 }
